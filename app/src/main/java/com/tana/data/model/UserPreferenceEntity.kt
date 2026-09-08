47d97ce8d62d2fdcd084d6e60057461b993d9a67
NexVior-arch <senandikastore@gmail.com>
2026-09-08 18:33:30 +0700

    Upload app/src/main/java/com/tana/data/model/UserPreferenceEntity.kt

---
+++ app/src/main/java/com/tana/data/model/UserPreferenceEntity.kt
@@ -0,0 +1,11 @@
+package com.tana.data.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "user_preferences")
+data class UserPreferenceEntity(
+    @PrimaryKey
+    val key: String,
+    val value: String
+)
