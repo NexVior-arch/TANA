5ea5893c5da6a0dffd3468d057db3f81ec4dd2bd
NexVior-arch <senandikastore@gmail.com>
2026-09-08 18:34:15 +0700

    Upload app/src/main/java/com/tana/widget/SavingsSmallWidgetProvider.kt

---
+++ app/src/main/java/com/tana/widget/SavingsSmallWidgetProvider.kt
@@ -0,0 +1,16 @@
+package com.tana.widget
+
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+
+class SavingsSmallWidgetProvider : AppWidgetProvider() {
+
+    override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
+        WidgetUpdateHelper.updateAllWidgets(context)
+    }
+
+    override fun onEnabled(context: Context) {
+        WidgetUpdateHelper.updateAllWidgets(context)
+    }
+}
